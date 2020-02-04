653b2bba21c79a52f52210b512414a08833327e5
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-02-04 16:52:43 +0100

    More config.

--- app/config/config.go
+++ app/config/config.go
@@ -1,13 +1,17 @@
 package config
 
 import (
+	"fmt"
 	log "github.com/golang/glog"
 	"github.com/jinzhu/configor"
 	"github.com/timshannon/bolthold"
+	"strconv"
 )
 
 type Config struct {
-	Db struct {
+	Port int    `default:"8080" env:"KITHUB_PORT"`
+	Addr string `default:"" env:"KITHUB_ADDR"`
+	Db   struct {
 		Path string `default:"kithub.db" env:"KITHUB_DB_FILE"`
 	}
 }
@@ -28,6 +32,8 @@ func Init() {
 		log.Fatalf("failed to load config file — %v\n", err)
 	}
 
+	fmt.Println(config.Port)
+
 	// Init database
 	if _db, err := bolthold.Open(config.Db.Path, 0600, nil); err != nil {
 		log.Fatalf("failed to open database — %v\n", err)
@@ -42,4 +48,8 @@ func Get() *Config {
 
 func Db() *bolthold.Store {
 	return db
+}
+
+func (c *Config) ListenAddr() string {
+	return c.Addr + ":" + strconv.Itoa(c.Port)
 }
