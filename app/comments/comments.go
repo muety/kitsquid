1f1cc1f2dd39a84f9057b7e571efff3cd5891179
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-03-28 13:15:26 +0100

    Show overall average rating even when logged out.

--- app/comments/comments.go
+++ app/comments/comments.go
@@ -2,6 +2,8 @@ package comments
 
 import "github.com/timshannon/bolthold"
 
+// TODO: Refactor comments to be part of events package, analogously to reviews
+
 func Init(store *bolthold.Store) {
 	InitStore(store)
 }
